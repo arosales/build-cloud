e9a80b679c53eca169f50687a3c582f443b9d44c
Seman Said <seman@users.noreply.github.com>
2016-03-17 17:54:01 -0700

    Added CWR job scheduler.

---
+++ buildcloud/schedule_cwr_jobs.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+from collections import namedtuple
+import os
+import yaml
+
+from jenkins import Jenkins
+
+
+Credentials = namedtuple('Credentials', ['user', 'password'])
+
+
+def parse_args(argv=None):
+    parser = ArgumentParser()
+    parser.add_argument(
+        'test_plan_dir', help='File path to test plan directory.')
+    parser.add_argument(
+        '--user', default=os.environ.get('JENKINS_USER'))
+    parser.add_argument(
+        '--password', default=os.environ.get('JENKINS_PASSWORD'))
+    parser.add_argument(
+        'controllers', nargs='+', help='List of controllers. ')
+    parser.add_argument(
+        '--test_plans', nargs='+',
+        help='List of test plan files.  Instead of scheduling all the tests, '
+             'this can be use to restrict the test plan files. If this is '
+             'not set, all the test will be scheduled.')
+    return parser.parse_args(argv)
+
+
+def make_parameters(test_plan, args):
+    with open(test_plan, 'r') as f:
+        plan = yaml.load(f)
+    parameters = {
+        'test_plan': test_plan,
+        'controllers': args.controllers,
+        'bundle_name': plan['bundle_name'],
+        'bundle_file': plan.get('bundle_file')
+    }
+    # Remove empty values
+    parameters = {k: v for k, v in parameters.items() if v}
+    return parameters
+
+
+def make_jobs(args):
+    test_plans = args.test_plans or os.listdir(args.test_plan_dir)
+    for test_plan in test_plans:
+        if not test_plan.endswith('.yaml'):
+            continue
+        test_plan = os.path.join(args.test_plan_dir, test_plan)
+        yield make_parameters(test_plan, args)
+
+
+def get_credentials(args):
+    if None in (args.user, args.password):
+        raise ValueError(
+            'Jenkins username and/or password not supplied.')
+    return Credentials(args.user, args.password)
+
+
+def build_jobs(credentials, jobs):
+    jenkins = Jenkins('http://localhost:8080', *credentials)
+    for job in jobs:
+        jenkins.build_job('cwr-test', job)
+
+
+def main():
+    args = parse_args()
+    credentials = get_credentials(args)
+    jobs = make_jobs(args)
+    build_jobs(credentials, jobs)
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_schedule_cwr_jobs.py
@@ -0,0 +1,118 @@
+from argparse import Namespace
+from contextlib import contextmanager
+import os
+from unittest import TestCase
+
+from mock import (
+    patch,
+    call,
+)
+import yaml
+
+from buildcloud.schedule_cwr_jobs import (
+    build_jobs,
+    Credentials,
+    get_credentials,
+    make_jobs,
+    make_parameters,
+    parse_args
+)
+from buildcloud.utility import temp_dir
+
+
+class TestSchedule(TestCase):
+
+    def test_parse_args(self):
+        with jenkins_env():
+            args = parse_args(['test_dir', 'default-aws', 'default-azure'])
+            expected = Namespace(
+                controllers=['default-aws', 'default-azure'],
+                password='bar',
+                test_plan_dir='test_dir', test_plans=None, user='foo')
+            self.assertEqual(args, expected)
+
+    def test_make_parameters(self):
+        with temp_dir() as test_dir:
+            test_plan = self.fake_parameters(test_dir)
+            args = Namespace(controllers=['default-aws'])
+            parameters = make_parameters(test_plan, args)
+            expected = {
+                'controllers': ['default-aws'],
+                'bundle_name': 'make_life_easy',
+                'test_plan': test_plan}
+            self.assertEqual(parameters, expected)
+
+    def test_make_jobs(self):
+        args = Namespace(controllers=['default-aws'], password='bar',
+                         test_plan_dir='', test_plans=None, user='foo')
+        with temp_dir() as test_dir:
+            args.test_plan_dir = test_dir
+            test_plan = self.fake_parameters(test_dir)
+            test_plan_2 = self.fake_parameters(test_dir, 2)
+            self.fake_parameters(test_dir, 3, ext='.py')
+            parameters = list(make_jobs(args))
+            expected = [
+                {'controllers': ['default-aws'],
+                 'bundle_name': 'make_life_easy',
+                 'test_plan': test_plan},
+                {'controllers': ['default-aws'],
+                 'bundle_name': 'make_life_easy',
+                 'test_plan': test_plan_2}]
+            self.assertItemsEqual(parameters, expected)
+
+    def test_get_credentials(self):
+        args = Namespace(controllers=['default-aws'], password='bar',
+                         test_plan_dir='', test_plans=None, user='foo')
+        cred = get_credentials(args)
+        self.assertEqual(cred.user, 'foo')
+        self.assertEqual(cred.password, 'bar')
+
+    def fake_parameters(self, test_dir, count=1, ext='.yaml'):
+        test_plan = os.path.join(test_dir, 'test' + str(count) + ext)
+        plan = {
+            'bundle': 'make life easy',
+            'bundle_name': 'make_life_easy',
+            'bundle_file': ''
+        }
+        with open(test_plan, 'w') as f:
+            yaml.dump(plan, f)
+        return test_plan
+
+    def test_build_jobs_credentials(self):
+        credentials = Credentials('joe', 'pass')
+        params = [
+            {'controllers': ['default-aws'],
+             'bundle_name': 'make_life_easy',
+             'test_plan': 'test_plan'},
+            {'controllers': ['default-aws'],
+             'bundle_name': 'made life easy',
+             'test_plan': 'test_plan_2'}]
+        with patch('buildcloud.schedule_cwr_jobs.Jenkins',
+                   autospec=True) as jenkins_mock:
+            build_jobs(credentials, params)
+        jenkins_mock.assert_called_once_with(
+            'http://localhost:8080', 'joe', 'pass')
+        calls = [
+            call('cwr-test',
+                 {'controllers': ['default-aws'],
+                  'bundle_name': 'make_life_easy',
+                  'test_plan': 'test_plan'}),
+            call('cwr-test',
+                 {'controllers': ['default-aws'],
+                  'bundle_name': 'made life easy',
+                  'test_plan': 'test_plan_2'})]
+        self.assertEqual(
+            jenkins_mock.return_value.build_job.call_args_list, calls)
+
+
+@contextmanager
+def jenkins_env():
+    user = os.environ.get('JENKINS_USER')
+    password = os.environ.get('JENKINS_PASSWORD')
+    os.environ["JENKINS_USER"] = 'foo'
+    os.environ["JENKINS_PASSWORD"] = 'bar'
+    try:
+        yield
+    finally:
+        os.environ["JENKINS_USER"] = user if user else ''
+        os.environ["JENKINS_PASSWORD"] = password if password else ''
